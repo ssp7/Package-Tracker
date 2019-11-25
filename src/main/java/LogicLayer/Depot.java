535792bfbaab5074b1ec29039121c2fa8670427b
Mkuhry <mkuhry@huskers.unl.edu>
2019-11-25 15:29:03 -0600

    Updating Depot and Location Class

--- src/main/java/LogicLayer/Depot.java
+++ src/main/java/LogicLayer/Depot.java
@@ -1,4 +1,44 @@
 package LogicLayer;
 
+import java.util.List;
+import java.util.Queue;
+
+
 public class Depot {
+
+    public Depot(List<Drone> droneList, Queue<Package> packageQueue, Location depotLocation) {
+        this.droneList = droneList;
+        this.packageQueue = packageQueue;
+        this.depotLocation = depotLocation;
+    }
+
+    private List<Drone> droneList;
+    private Queue<Package> packageQueue;
+    private Location depotLocation;
+
+    public List<Drone> getDroneList() {
+        return droneList;
+    }
+
+    public void setDroneList(List<Drone> droneList) {
+        this.droneList = droneList;
+    }
+
+    public Queue<Package> getPackageQueue() {
+        return packageQueue;
+    }
+
+    public void setPackageQueue(Queue<Package> packageQueue) {
+        this.packageQueue = packageQueue;
+    }
+
+    public Location getDepotLocation() {
+        return depotLocation;
+    }
+
+    public void setDepotLocation(Location depotLocation) {
+        this.depotLocation = depotLocation;
+    }
+
+
 }
--- src/main/java/LogicLayer/Location.java
+++ src/main/java/LogicLayer/Location.java
@@ -1,4 +1,27 @@
 package LogicLayer;
 
 public class Location {
+
+    public Location(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+    private double longitude;
+    private double latitude;
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
 }

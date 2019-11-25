2635ac7b94d61f2af3c5561eb34af7d483dda357
Ryan <rsthomas@huskers.unl.edu>
2019-11-25 15:31:03 -0600

    Updating Package class to implement methods and attributes.

--- src/main/java/LogicLayer/Package.java
+++ src/main/java/LogicLayer/Package.java
@@ -1,4 +1,49 @@
 package LogicLayer;
 
 public class Package {
+
+    public Package(String orderNumber, Location destination, Location origin, String status) {
+        OrderNumber = orderNumber;
+        this.destination = destination;
+        this.origin = origin;
+        this.status = status;
+    }
+
+    private String OrderNumber;
+    private Location destination;
+    private Location origin;
+    private String status;
+
+    public String getOrderNumber() {
+        return OrderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        OrderNumber = orderNumber;
+    }
+
+    public Location getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Location destination) {
+        this.destination = destination;
+    }
+
+    public Location getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Location origin) {
+        this.origin = origin;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }

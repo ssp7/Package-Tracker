f8e4bc68b467330cf0fdfe779a6668d0400e3a91
Ryan <rsthomas@huskers.unl.edu>
2019-12-03 16:25:25 -0600

    Added the front end for checking status of drones and packages.

---
+++ src/main/java/edu/unl/cse/csce361/package_tracker/Commands/CheckDroneStatus.java
@@ -0,0 +1,13 @@
+package edu.unl.cse.csce361.package_tracker.Commands;
+
+public class CheckDroneStatus implements Command {
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Check Drone Status";
+    }
+}
---
+++ src/main/java/edu/unl/cse/csce361/package_tracker/Commands/CheckPackageStatus.java
@@ -0,0 +1,13 @@
+package edu.unl.cse.csce361.package_tracker.Commands;
+
+public class CheckPackageStatus implements Command {
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Check Package Status";
+    }
+}

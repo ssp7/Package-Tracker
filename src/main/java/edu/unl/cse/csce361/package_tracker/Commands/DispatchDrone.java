8ce69a27c307cd19f7c09c4139729e876b360d17
Ryan <rsthomas@huskers.unl.edu>
2019-12-08 19:01:47 -0600

    Linked DispatchDrone command to it's corresponding function in drone manager.

--- src/main/java/edu/unl/cse/csce361/package_tracker/Commands/DispatchDrone.java
+++ src/main/java/edu/unl/cse/csce361/package_tracker/Commands/DispatchDrone.java
@@ -1,10 +1,12 @@
 package edu.unl.cse.csce361.package_tracker.Commands;
 
+import edu.unl.cse.csce361.package_tracker.LogicLayer.DroneManager;
+
 public class DispatchDrone implements Command {
 
     @Override
     public void execute() {
-
+        DroneManager.droneDispatch();
     }
     @Override
     public String toString(){
